81bf896098fd9b6a1a7b40275667ae107fe82caa
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-19 14:50:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 var destructivelyAppendKitten = kittens.push("Dorota");
-return kittens.concat(destructivelyAppendKitten);
+return kittens.concat();
